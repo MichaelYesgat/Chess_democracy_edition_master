df2a717ffbee38ca71d668763c47d18bcdd80d5c
MichaelYesgat <98889472+MichaelYesgat@users.noreply.github.com>
2025-09-10 15:18:57 -0500

    Delete src/main/java/com/chess/democracy/edition directory

--- src/main/java/com/chess/democracy/edition/networking/GameClient.java
+++
@@ -1,6 +0,0 @@
-package com.chess.democracy.edition.networking;
-
-public class GameClient {
-    //to be implemented
-
-}
--- src/main/java/com/chess/democracy/edition/src/main/java/com/chess/democracy/edition/SceneChanger.java
+++
@@ -1,7 +0,0 @@
-package com.chess.democracy.edition;
-
-import javafx.event.ActionEvent;
-
-public interface SceneChanger {
-     void changeScene(ActionEvent event, String fxmlFile, String title);
-}
